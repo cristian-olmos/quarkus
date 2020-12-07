81f5b872cfefbfba5ee52a6a46725e997b5a6797
Cristian Olmos <cristian.olmos@melia.com>
2020-12-07 12:21:58 +0100

    Initial commit

---
+++ src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user
+(
+  id   INT,
+  name VARCHAR(20)
+);
+INSERT INTO user(id, name)
+VALUES (1, 'cristian@pruebas.com');
---
+++ src/main/resources/db/migration/V1.0.1__User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users
+(
+  id   INT,
+  name VARCHAR(20)
+);
+INSERT INTO users(id, name)
+VALUES (1, 'cristian@pruebas.com');
